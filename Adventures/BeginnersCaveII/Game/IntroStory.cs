5256547dd881c6eda0fae5369f1436f52bb76edc
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-04-11 21:23:52 -0700

    Working on new adventure: Beginner's Cave II

---
+++ Adventures/BeginnersCaveII/Game/IntroStory.cs
@@ -0,0 +1,55 @@
+﻿
+// IntroStory.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Diagnostics;
+using Eamon.Game.Attributes;
+using EamonRT.Framework;
+using EamonRT.Framework.Primitive.Enums;
+using static BeginnersCaveII.Game.Plugin.PluginContext;
+
+namespace BeginnersCaveII.Game
+{
+	[ClassMappings]
+	public class IntroStory : EamonRT.Game.IntroStory, IIntroStory
+	{
+		public override void PrintOutputBeginnersPrelude()
+		{
+			gEngine.PrintEffectDesc(3);
+
+			var room = gRDB[gEngine.StartRoom];
+
+			Debug.Assert(room != null);
+
+			gOut.Print("{0}", room.Desc);
+
+			gEngine.PrintEffectDesc(5);
+		}
+
+		public override void PrintOutputBeginnersTooManyWeapons()
+		{
+			gEngine.PrintEffectDesc(8);
+		}
+
+		public override void PrintOutputBeginnersNoWeapons()
+		{
+			gEngine.PrintEffectDesc(4);
+		}
+
+		public override void PrintOutputBeginnersNotABeginner()
+		{
+			gEngine.PrintEffectDesc(6);
+		}
+
+		public override void PrintOutputBeginnersMayNowProceed()
+		{
+			gEngine.PrintEffectDesc(7);
+		}
+
+		public IntroStory()
+		{
+			StoryType = IntroStoryType.Beginners;
+		}
+	}
+}
--- Adventures/BeginnersCaveII/Game/Plugin/PluginContext.cs
+++ Adventures/BeginnersCaveII/Game/Plugin/PluginContext.cs
@@ -3,6 +3,9 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using Eamon.Framework.DataStorage.Generic;
+using Eamon.Framework.Portability;
+
 namespace BeginnersCaveII.Game.Plugin
 {
 	public static class PluginContext
@@ -19,11 +22,27 @@ namespace BeginnersCaveII.Game.Plugin
 			}
 		}
 
+		public static ITextWriter gOut
+		{
+			get
+			{
+				return EamonRT.Game.Plugin.PluginContext.gOut;
+			}
+		}
+
 		public static EamonRT.Framework.IEngine gEngine
 		{
 			get
 			{
 				return (EamonRT.Framework.IEngine)EamonRT.Game.Plugin.PluginContext.gEngine;
+			}
+		}
+
+		public static IRecordDb<Eamon.Framework.IRoom> gRDB
+		{
+			get
+			{
+				return (IRecordDb<Eamon.Framework.IRoom>)EamonRT.Game.Plugin.PluginContext.gRDB;
 			}
 		}
 	}
