dc655da81fb90922684d262a7fc432939ab4259b
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-04-11 19:43:06 -0700

    Working on new adventure: Beginner's Cave II

--- Adventures/BeginnersCaveII/Game/Engine.cs
+++ Adventures/BeginnersCaveII/Game/Engine.cs
@@ -3,6 +3,7 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System.Collections.Generic;
 using Eamon.Framework;
 using Eamon.Game.Attributes;
 using static BeginnersCaveII.Game.Plugin.PluginContext;
@@ -12,6 +13,20 @@ namespace BeginnersCaveII.Game
 	[ClassMappings(typeof(IEngine))]
 	public class Engine : EamonRT.Game.Engine, EamonRT.Framework.IEngine
 	{
+		public override void InitArtifacts()
+		{
+			base.InitArtifacts();
 
+			var synonyms = new Dictionary<long, string[]>()
+			{
+				{ 21, new string[] { "book", "shelf", "shelves" } },
+				{ 22, new string[] { "rock", "wall", "south wall" } },
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateArtifactSynonyms(synonym.Key, synonym.Value);
+			}
+		}
 	}
 }
