8361ffd40dd8cbc5cf43966dbc9dee7692f96d3a
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-04-13 19:29:16 -0700

    Working on new adventure: Alternate Beginner's Cave

--- Adventures/AlternateBeginnersCave/Game/Engine.cs
+++ Adventures/AlternateBeginnersCave/Game/Engine.cs
@@ -3,7 +3,11 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Eamon.Framework;
+using Eamon.Framework.Primitive.Classes;
 using Eamon.Game.Attributes;
 using static AlternateBeginnersCave.Game.Plugin.PluginContext;
 
@@ -12,6 +16,51 @@ namespace AlternateBeginnersCave.Game
 	[ClassMappings(typeof(IEngine))]
 	public class Engine : EamonRT.Game.Engine, EamonRT.Framework.IEngine
 	{
+		/// <summary></summary>
+		public virtual long HeldWpnIdx { get; set; }
 
+		public override void AddUniqueCharsToArtifactAndMonsterNames()
+		{
+			base.AddUniqueCharsToArtifactAndMonsterNames();
+
+			var signArtifact = gADB[23];     // Sign #2
+
+			Debug.Assert(signArtifact != null);
+
+			signArtifact.Name = signArtifact.Name.TrimEnd('#');
+		}
+
+		public override IArtifact ConvertWeaponToArtifact(ICharacterArtifact weapon)
+		{
+			var artifact = base.ConvertWeaponToArtifact(weapon);
+
+			var i = Array.FindIndex(gCharacter.Weapons, x => x == weapon);
+
+			if (i != gGameState.UsedWpnIdx)
+			{
+				artifact.SetInLimbo();
+
+				gGameState.SetHeldWpnUids(HeldWpnIdx++, artifact.Uid);
+			}
+
+			return artifact;
+		}
+
+		public override void ConvertToCarriedInventory(IList<IArtifact> weaponList)
+		{
+			for (var i = 0; i < gGameState.HeldWpnUids.Length; i++)
+			{
+				if (gGameState.GetHeldWpnUids(i) > 0)
+				{
+					var artifact = gADB[gGameState.GetHeldWpnUids(i)];
+
+					Debug.Assert(artifact != null);
+
+					artifact.SetCarriedByCharacter();
+				}
+			}
+
+			base.ConvertToCarriedInventory(weaponList);
+		}
 	}
 }
---
+++ Adventures/AlternateBeginnersCave/Game/IntroStory.cs
@@ -0,0 +1,55 @@
+﻿
+// IntroStory.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Diagnostics;
+using Eamon.Game.Attributes;
+using EamonRT.Framework;
+using EamonRT.Framework.Primitive.Enums;
+using static AlternateBeginnersCave.Game.Plugin.PluginContext;
+
+namespace AlternateBeginnersCave.Game
+{
+	[ClassMappings]
+	public class IntroStory : EamonRT.Game.IntroStory, IIntroStory
+	{
+		public override void PrintOutputBeginnersPrelude()
+		{
+			gEngine.PrintEffectDesc(14);
+
+			var room = gRDB[gEngine.StartRoom];
+
+			Debug.Assert(room != null);
+
+			gOut.Print("{0}", room.Desc);
+
+			gEngine.PrintEffectDesc(16);
+		}
+
+		public override void PrintOutputBeginnersTooManyWeapons()
+		{
+			gEngine.PrintEffectDesc(19);
+		}
+
+		public override void PrintOutputBeginnersNoWeapons()
+		{
+			gEngine.PrintEffectDesc(15);
+		}
+
+		public override void PrintOutputBeginnersNotABeginner()
+		{
+			gEngine.PrintEffectDesc(17);
+		}
+
+		public override void PrintOutputBeginnersMayNowProceed()
+		{
+			gEngine.PrintEffectDesc(18);
+		}
+
+		public IntroStory()
+		{
+			StoryType = IntroStoryType.Beginners;
+		}
+	}
+}
