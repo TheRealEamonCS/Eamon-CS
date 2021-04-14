6c63477bf2b2c243ac42eeea55c68a7335dedb0c
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-04-13 20:59:51 -0700

    Working on new adventure: Alternate Beginner's Cave

---
+++ Adventures/AlternateBeginnersCave/Game/Commands/Player/Manipulation/ReadCommand.cs
@@ -0,0 +1,46 @@
+﻿
+// ReadCommand.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System.Diagnostics;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Commands;
+using EamonRT.Framework.States;
+using static AlternateBeginnersCave.Game.Plugin.PluginContext;
+
+namespace AlternateBeginnersCave.Game.Commands
+{
+	[ClassMappings]
+	public class ReadCommand : EamonRT.Game.Commands.ReadCommand, IReadCommand
+	{
+		public override void Execute()
+		{
+			Debug.Assert(DobjArtifact != null);
+
+			// Unremarkable box
+
+			if (DobjArtifact.Uid == 7)
+			{
+				gEngine.PrintEffectDesc(13);
+
+				NextState = Globals.CreateInstance<IMonsterStartState>();
+			}
+
+			// Sign in first room
+
+			else if (DobjArtifact.Uid == 23)
+			{
+				var command = Globals.CreateInstance<IExamineCommand>();
+
+				CopyCommandData(command);
+
+				NextState = command;
+			}
+			else
+			{
+				base.Execute();
+			}
+		}
+	}
+}
