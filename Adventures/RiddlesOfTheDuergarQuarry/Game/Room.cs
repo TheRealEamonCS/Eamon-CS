0b30826d852fd58c846a12314721f9e5c89ca0d0
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-04-21 18:49:18 -0700

    More work on Riddles of the Duergar Quarry.

--- Adventures/RiddlesOfTheDuergarQuarry/Game/Room.cs
+++ Adventures/RiddlesOfTheDuergarQuarry/Game/Room.cs
@@ -3,6 +3,7 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System.Linq;
 using Eamon.Framework;
 using Eamon.Game.Attributes;
 using static RiddlesOfTheDuergarQuarry.Game.Plugin.PluginContext;
@@ -12,6 +13,16 @@ namespace RiddlesOfTheDuergarQuarry.Game
 	[ClassMappings]
 	public class Room : Eamon.Game.Room, IRoom
 	{
+		public override bool IsDirectionInObviousExitsList(long index)
+		{
+			var roomUids = new long[] 
+			{ 
+				49, 50, 51, 52, 
+			};
 
+			// Suppress up for all rooms in roomUids; suppress down for all except room Uid 49
+
+			return base.IsDirectionInObviousExitsList(index) && (roomUids.Contains(Uid) ? index != 5 && (Uid == 49 || index != 6) : true);
+		}
 	}
 }
