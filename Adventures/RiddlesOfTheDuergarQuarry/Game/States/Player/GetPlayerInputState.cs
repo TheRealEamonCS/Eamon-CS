6d6d965c92651716982ad24966590faffd944bb8
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-04-22 19:35:58 -0700

    More work on Riddles of the Duergar Quarry.

---
+++ Adventures/RiddlesOfTheDuergarQuarry/Game/States/Player/GetPlayerInputState.cs
@@ -0,0 +1,17 @@
+﻿
+// GetPlayerInputState.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using Eamon.Game.Attributes;
+using EamonRT.Framework.States;
+using static RiddlesOfTheDuergarQuarry.Game.Plugin.PluginContext;
+
+namespace RiddlesOfTheDuergarQuarry.Game.States
+{
+	[ClassMappings]
+	public class GetPlayerInputState : EamonRT.Game.States.GetPlayerInputState, IGetPlayerInputState
+	{
+
+	}
+}
---
+++ Adventures/RiddlesOfTheDuergarQuarry/Game/States/Player/PrintPlayerRoomState.cs
@@ -0,0 +1,17 @@
+﻿
+// PrintPlayerRoomState.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using Eamon.Game.Attributes;
+using EamonRT.Framework.States;
+using static RiddlesOfTheDuergarQuarry.Game.Plugin.PluginContext;
+
+namespace RiddlesOfTheDuergarQuarry.Game.States
+{
+	[ClassMappings]
+	public class PrintPlayerRoomState : EamonRT.Game.States.PrintPlayerRoomState, IPrintPlayerRoomState
+	{
+
+	}
+}
