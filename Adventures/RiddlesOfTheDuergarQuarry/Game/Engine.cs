32069922383ee87d16a67c724b5bdd6f0a564eb2
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-04-23 19:58:48 -0700

    More work on Riddles of the Duergar Quarry.

--- Adventures/RiddlesOfTheDuergarQuarry/Game/Engine.cs
+++ Adventures/RiddlesOfTheDuergarQuarry/Game/Engine.cs
@@ -3,6 +3,8 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System.Collections.Generic;
+using System.Linq;
 using Eamon.Framework;
 using Eamon.Game.Attributes;
 using static RiddlesOfTheDuergarQuarry.Game.Plugin.PluginContext;
@@ -12,6 +14,67 @@ namespace RiddlesOfTheDuergarQuarry.Game
 	[ClassMappings(typeof(IEngine))]
 	public class Engine : EamonRT.Game.Engine, EamonRT.Framework.IEngine
 	{
+		public override void InitArtifacts()
+		{
+			base.InitArtifacts();
 
+			MacroFuncs.Add(1, () =>
+			{
+				var roomUids = new long[] { };
+
+				var result = "east and west";
+
+				if (gGameState != null && roomUids.Contains(gGameState.Ro))
+				{
+					result = "north and south";
+				}
+
+				return result;
+			});
+
+			MacroFuncs.Add(2, () =>
+			{
+				var result = "northeast";
+
+				if (gGameState != null)
+				{
+					// TODO: implement
+				}
+
+				return result;
+			});
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+				{ 1, new string[] { "everdes", "volcano", "mountain" } },
+				{ 2, new string[] { "purple lavender", "sage", "lavender" } },
+				{ 3, new string[] { "dust devils", "devils", "whirlwinds", "devil", "whirlwind" } },
+				{ 5, new string[] { "fence" } },
+				{ 14, new string[] { "gate" } },
+				{ 77, new string[] { "narrow crevice", "vertical crevice", "crevice", "crevace", "fissure" } },
+				{ 78, new string[] { "large rock", "boulder", "rock" } },
+				{ 79, new string[] { "bell", "chime", "hammer" } },
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateArtifactSynonyms(synonym.Key, synonym.Value);
+			}
+		}
+
+		public override void InitMonsters()
+		{
+			base.InitMonsters();
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+				{ 18, new string[] { "viper", "snake" } },
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateMonsterSynonyms(synonym.Key, synonym.Value);
+			}
+		}
 	}
 }
---
+++ Adventures/RiddlesOfTheDuergarQuarry/Game/Parsing/CommandParser.cs
@@ -0,0 +1,20 @@
+﻿
+// CommandParser.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Parsing;
+using static RiddlesOfTheDuergarQuarry.Game.Plugin.PluginContext;
+
+namespace RiddlesOfTheDuergarQuarry.Game.Parsing
+{
+	[ClassMappings]
+	public class CommandParser : EamonRT.Game.Parsing.CommandParser, ICommandParser
+	{
+		public virtual void FinishParsingClimbCommand()
+		{
+			ResolveRecord(false);
+		}
+	}
+}
