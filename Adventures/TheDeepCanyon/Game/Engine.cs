7af9b89522902c7e2c9af74a60f8948f5c3a5f5f
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-05-16 07:49:36 -0700

    Working on new adventure: The Deep Canyon

--- Adventures/TheDeepCanyon/Game/Engine.cs
+++ Adventures/TheDeepCanyon/Game/Engine.cs
@@ -3,6 +3,8 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System.Collections.Generic;
+using System.Diagnostics;
 using Eamon.Framework;
 using Eamon.Game.Attributes;
 using static TheDeepCanyon.Game.Plugin.PluginContext;
@@ -12,6 +14,26 @@ namespace TheDeepCanyon.Game
 	[ClassMappings(typeof(IEngine))]
 	public class Engine : EamonRT.Game.Engine, EamonRT.Framework.IEngine
 	{
+		public override void InitArtifacts()
+		{
+			base.InitArtifacts();
 
+			MacroFuncs.Add(1, () =>
+			{
+				// silver lantern
+
+				return gGameState != null && gGameState.Ls == 7 ? "lit" : "unlit";
+			});
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateArtifactSynonyms(synonym.Key, synonym.Value);
+			}
+		}
 	}
 }
