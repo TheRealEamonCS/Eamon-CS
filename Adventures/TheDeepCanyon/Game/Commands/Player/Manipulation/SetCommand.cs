bf114920e6c8f0e34c9a068da2805671d2f6dc76
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2021-06-14 22:42:22 -0700

    Working on new adventure: The Deep Canyon

--- Adventures/TheDeepCanyon/Game/Commands/Player/Manipulation/SetCommand.cs
+++ Adventures/TheDeepCanyon/Game/Commands/Player/Manipulation/SetCommand.cs
@@ -44,9 +44,12 @@ namespace TheDeepCanyon.Game.Commands
 				}
 				else
 				{
-					if (mouseArtifact.IsCarriedByContainer(DobjArtifact))
+					if (!mouseArtifact.IsInLimbo())
 					{
-						gOut.Print("The mouse escapes as you set the trap.");
+						if (mouseArtifact.IsCarriedByCharacter() || mouseArtifact.IsCarriedByContainer(DobjArtifact) || mouseArtifact.IsInRoom(ActorRoom))
+						{
+							gOut.Print("The mouse escapes as you set the trap.");
+						}
 
 						mouseArtifact.SetInLimbo();
 					}
