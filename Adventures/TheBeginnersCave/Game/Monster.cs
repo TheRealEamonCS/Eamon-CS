61a01915387ae46f1196ea30396dac00301e2de2
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-02-12 12:04:52 -0800

    Bug fix: in The Beginner's Cave, tightened up the Trollsfire effect.

--- Adventures/TheBeginnersCave/Game/Monster.cs
+++ Adventures/TheBeginnersCave/Game/Monster.cs
@@ -24,31 +24,33 @@ namespace TheBeginnersCave.Game
 
 			set
 			{
-				if (Globals.EnableGameOverrides && base.Weapon != value)
+				if (Globals.EnableGameOverrides && gGameState != null)
 				{
-					Debug.Assert(gGameState != null);
+					// if this is the character monster and Trollsfire is being wielded
 
-					// if this is the character monster or the pirate
-
-					if (Uid == gGameState.Cm || Uid == 8)
+					if (Uid == gGameState.Cm && base.Weapon == 10)
 					{
-						// if going from wielding Trollsfire to not wielding Trollsfire and the Trollsfire effect is active
+						// deactivate Trollsfire effect; the Trollsfire property is complex and does a fair bit of processing
 
-						if (base.Weapon == 10 && value != 10 && gGameState.Trollsfire == 1)
-						{
-							// deactivate Trollsfire effect; the Trollsfire property is complex and does a fair bit of processing
-
-							gGameState.Trollsfire = 0;
-						}
+						gGameState.Trollsfire = 0;
 					}
 
 					// if this is the pirate
 
-					if (Uid == 8)
+					else if (Uid == 8)
 					{
-						// if going from not wielding Trollsfire to wielding Trollsfire and the Trollsfire effect is not active
+						// if going from wielding Trollsfire to not wielding Trollsfire
 
-						if (base.Weapon != 10 && value == 10 && gGameState.Trollsfire == 0)
+						if (base.Weapon == 10 && value != 10)
+						{
+							// deactivate Trollsfire effect
+
+							gGameState.Trollsfire = 0;
+						}
+
+						// if going from not wielding Trollsfire to wielding Trollsfire
+
+						else if (base.Weapon != 10 && value == 10)
 						{
 							// activate Trollsfire effect
 
