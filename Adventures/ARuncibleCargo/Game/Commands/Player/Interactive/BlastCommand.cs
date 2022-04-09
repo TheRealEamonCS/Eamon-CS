1c7b286839338cf67c71b6aa10a30cd1aafff892
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-04-09 00:56:44 -0700

    Bug fix: for A Runcible Cargo, normalized text output in AttackCommand and BlastCommand.

--- Adventures/ARuncibleCargo/Game/Commands/Player/Interactive/BlastCommand.cs
+++ Adventures/ARuncibleCargo/Game/Commands/Player/Interactive/BlastCommand.cs
@@ -3,8 +3,10 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System.Diagnostics;
 using Eamon.Game.Attributes;
 using EamonRT.Framework.Commands;
+using EamonRT.Framework.States;
 using static ARuncibleCargo.Game.Plugin.PluginContext;
 
 namespace ARuncibleCargo.Game.Commands
@@ -12,6 +14,24 @@ namespace ARuncibleCargo.Game.Commands
 	[ClassMappings]
 	public class BlastCommand : EamonRT.Game.Commands.BlastCommand, IBlastCommand
 	{
+		public override void Execute()
+		{
+			Debug.Assert(DobjArtifact != null || DobjMonster != null);
+
+			// Can't blast the Runcible Cargo
+
+			if (DobjArtifact != null && DobjArtifact.Uid == 129)
+			{
+				gOut.Print("That sounds quite dangerous!");
+
+				NextState = Globals.CreateInstance<IMonsterStartState>();
+			}
+			else
+			{
+				base.Execute();
+			}
+		}
+
 		public override bool IsAllowedInRoom()
 		{
 			// Disable BlastCommand in water rooms
