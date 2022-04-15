5cd3cdccd2d4c00cd155f978c53f894452f8cb18
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-04-14 17:13:05 -0700

    Bug fix: in The Training Ground, refactored logic relating to the backpack.

--- Adventures/TheTrainingGround/Game/Commands/Player/Interactive/BlastCommand.cs
+++ Adventures/TheTrainingGround/Game/Commands/Player/Interactive/BlastCommand.cs
@@ -3,8 +3,11 @@
 
 // Copyright (c) 2014+ by Michael Penner.  All rights reserved.
 
+using System.Diagnostics;
+using Eamon.Framework.Primitive.Enums;
 using Eamon.Game.Attributes;
 using EamonRT.Framework.Commands;
+using EamonRT.Framework.States;
 using static TheTrainingGround.Game.Plugin.PluginContext;
 
 namespace TheTrainingGround.Game.Commands
@@ -12,6 +15,24 @@ namespace TheTrainingGround.Game.Commands
 	[ClassMappings]
 	public class BlastCommand : EamonRT.Game.Commands.BlastCommand, IBlastCommand
 	{
+		public override void Execute()
+		{
+			Debug.Assert(DobjArtifact != null || DobjMonster != null);
+
+			// BLAST backpack
+
+			if (gCharacter.GetSpellAbilities(Spell.Blast) > 0 && DobjArtifact != null && DobjArtifact.Uid == 13)
+			{
+				PrintDontNeedTo();
+
+				NextState = Globals.CreateInstance<IStartState>();
+			}
+			else
+			{
+				base.Execute();
+			}
+		}
+
 		public override bool ShouldAllowSkillGains()
 		{
 			// BLASTing Bozworth never increases skill
