65416970ff645b036e1ec5d8abd4afbc0d78f84e
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-06-11 12:12:33 -0700

    Bumped Eamon CS version to 2.1.0.  As always, existing datafiles are automatically upgraded.

---
+++ Adventures/AlternateBeginnersCave/Game/Monster.cs
@@ -0,0 +1,29 @@
+﻿
+// Monster.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using Eamon.Framework;
+using Eamon.Game.Attributes;
+using static AlternateBeginnersCave.Game.Plugin.PluginContext;
+
+namespace AlternateBeginnersCave.Game
+{
+	[ClassMappings]
+	public class Monster : Eamon.Game.Monster, IMonster
+	{
+		public override bool HasWornInventory()
+		{
+			// Only humanoids have a worn inventory list
+
+			return Uid != 6 && Uid != 7;
+		}
+
+		public override bool HasCarriedInventory()
+		{
+			// Only humanoids have a carried inventory list
+
+			return Uid != 6 && Uid != 7;
+		}
+	}
+}
