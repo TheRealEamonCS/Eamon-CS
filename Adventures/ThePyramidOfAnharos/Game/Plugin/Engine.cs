5e1970de38901e583838da47cf8dc0678fb211cf
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2022-11-25 17:24:32 -0800

    More work on The Pyramid of Anharos: added synonyms.

--- Adventures/ThePyramidOfAnharos/Game/Plugin/Engine.cs
+++ Adventures/ThePyramidOfAnharos/Game/Plugin/Engine.cs
@@ -33,13 +33,76 @@ namespace ThePyramidOfAnharos.Game.Plugin
 			return rc;
 		}
 
+		public override void InitArtifacts()
+		{
+			base.InitArtifacts();
+
+			var synonyms = new Dictionary<long, string[]>()
+			{
+				{ 3, new string[] { "sword" } },
+				{ 4, new string[] { "prod" } },
+				{ 6, new string[] { "rifle", "gun" } },
+				{ 7, new string[] { "rifle", "gun" } },
+				{ 10, new string[] { "scimitar" } },
+				{ 12, new string[] { "bag" } },
+				{ 21, new string[] { "wall", "flame" } },
+				{ 22, new string[] { "wall", "flame" } },
+				{ 23, new string[] { "cloud" } },
+				{ 24, new string[] { "cloud" } },
+				{ 25, new string[] { "body" } },
+				{ 26, new string[] { "body" } },
+				{ 27, new string[] { "body" } },
+				{ 28, new string[] { "door" } },
+				{ 29, new string[] { "body" } },
+				{ 31, new string[] { "moon pool", "pool" } },
+				{ 34, new string[] { "mummy", "Anharos" } },
+				{ 35, new string[] { "door" } },
+				{ 38, new string[] { "Diamond", "Purity" } },
+				{ 39, new string[] { "case" } },
+				{ 42, new string[] { "leather", "armor" } },
+				{ 43, new string[] { "chain", "armor" } },
+				{ 44, new string[] { "plate", "armor" } },
+				{ 47, new string[] { "caravan" } },
+				{ 48, new string[] { "water column", "column", "water" } },
+				{ 49, new string[] { "merchant", "Farouk" } },
+				{ 56, new string[] { "dead Omar", "dead body", "body", "Omar" } },
+				{ 57, new string[] { "dead Ali", "dead body", "body", "Ali" } },
+				{ 58, new string[] { "scorpion" } },
+				{ 59, new string[] { "dead devil", "dust devil", "devil" } },
+				{ 60, new string[] { "dead hermit", "crazed hermit", "hermit" } },
+				{ 61, new string[] { "dead Farouk", "dead body", "body", "Farouk" } },
+				{ 62, new string[] { "dead Saala el Kahir", "dead body", "body", "Saala", "el Kahir", "Kahir" } },
+				{ 64, new string[] { "dead Hamid", "dead body", "body", "Hamid" } },
+				{ 65, new string[] { "dead Abou", "dead body", "body", "Abou" } },
+				{ 66, new string[] { "dead Fouad", "dead body", "body", "Fouad" } },
+				{ 67, new string[] { "dead Mahomet", "dead body", "body", "Mahomet" } },
+				{ 68, new string[] { "dead Rahman", "dead body", "body", "Rahman" } },
+				{ 69, new string[] { "dead Yussuf", "dead body", "body", "Yussuf" } },
+				{ 70, new string[] { "dead Masoud", "dead body", "body", "Masoud" } },
+				{ 71, new string[] { "dead Farah", "dead body", "body", "Farah" } },
+				{ 72, new string[] { "dead Sheba", "dead body", "body", "Sheba" } },
+				{ 73, new string[] { "dead Basra", "dead body", "body", "Basra" } },
+				{ 74, new string[] { "dead children", "Riff children", "children", "dead Riff child", "dead child", "Riff child", "child" } },
+				{ 75, new string[] { "guards", "dead guard", "guard" } },
+			};
+
+			foreach (var synonym in synonyms)
+			{
+				CreateArtifactSynonyms(synonym.Key, synonym.Value);
+			}
+		}
+
 		public override void InitMonsters()
 		{
 			base.InitMonsters();
 
 			var synonyms = new Dictionary<long, string[]>()
 			{
-				// TODO
+				{ 4, new string[] { "devil" } },
+				{ 5, new string[] { "hermit" } },
+				{ 7, new string[] { "Saala", "el Kahir", "Kahir" } },
+				{ 8, new string[] { "avatar", "Alaxar" } },
+				{ 19, new string[] { "children", "child" } },
 			};
 
 			foreach (var synonym in synonyms)
@@ -71,20 +134,13 @@ namespace ThePyramidOfAnharos.Game.Plugin
 
 			Debug.Assert(room != null);
 
-			if (room.Uid > 0 && room.Uid < 6)
+			if (room.Uid > 0 && room.Uid < 6 && gGameState.GU > 0)
 			{
-				var guideMonster = MDB[1];
+				var guideMonster = MDB[gGameState.GU];
 
 				Debug.Assert(guideMonster != null);
 
-				if (!guideMonster.IsInRoom(room))
-				{
-					guideMonster = MDB[2];
-
-					Debug.Assert(guideMonster != null);
-				}
-
-				if (guideMonster.IsInRoom(room))
+				if (guideMonster.IsInRoom(room) && guideMonster.Reaction == Friendliness.Friend)
 				{
 					var direction = "";
 
