211be16cc84fa4a6ac5f550727bc5d142f551dc0
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-01-07 18:45:57 -0800

    More work on RulesetVersions 5 and 62.

---
+++ Adventures/TheTempleOfNgurct/Game/Components/ComponentImpl.cs
@@ -0,0 +1,26 @@
+﻿
+// ComponentImpl.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using System;
+using System.Diagnostics;
+using Eamon.Framework.Primitive.Classes;
+using Eamon.Framework.Primitive.Enums;
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Components;
+using static TheTempleOfNgurct.Game.Plugin.Globals;
+
+namespace TheTempleOfNgurct.Game.Components
+{
+	[ClassMappings]
+	public class ComponentImpl : EamonRT.Game.Components.ComponentImpl, IComponentImpl
+	{
+		public override void PrintSpellOverloadsBrain(Spell s, ISpell spell)
+		{
+			Debug.Assert(Enum.IsDefined(typeof(Spell), s) && spell != null);
+
+			gOut.Print("The strain of attempting to cast {0} overloads your brain and you forget it completely.", spell.Name);
+		}
+	}
+}
