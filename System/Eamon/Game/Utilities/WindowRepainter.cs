c546de560600d644c69bf71274592e972df8d55b
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2023-06-05 19:41:16 -0700

    Bug fix: more testing to determine the nature of the cursor artifact bug. Ultimately, some combination of cursor visibility, cursor position manipulation, console input/output, and timing is causing a sporadic console repaint failure. All attempts to fix this issue internally (within Eamon CS) have failed, so the only solution is to step outside the game engine and force the console window to repaint at strategic points in time. After putting the RepaintWindow method in a few critical spots, the bug has disappeared, but it may need to go in others, depending on the results of extended gameplay. The game documentation will be updated to remove the suggested workarounds (but you can continue to use them if you prefer, as they still work fine).

--- System/Eamon/Game/Utilities/WindowRepainter.cs
+++ System/Eamon/Game/Utilities/WindowRepainter.cs
@@ -8,36 +8,36 @@ using System.Runtime.InteropServices;
 
 namespace Eamon.Game.Utilities
 {
-    /// <summary>
-    /// </summary>
-    /// <remarks>
-    /// Full credit:  
-    ///     https://chat.openai.com/        "give me the C# code to repaint a window given its window handle"
-    /// </remarks>
-    public static class WindowRepainter
-    {
-        [DllImport("kernel32.dll")]
-        static extern IntPtr GetConsoleWindow();
+	/// <summary>
+	/// </summary>
+	/// <remarks>
+	/// Full credit:  
+	///     https://chat.openai.com/        "give me the C# code to repaint a window given its window handle"
+	/// </remarks>
+	public static class WindowRepainter
+	{
+		[DllImport("kernel32.dll")]
+		static extern IntPtr GetConsoleWindow();
 
-        [DllImport("user32.dll")]
-        static extern bool InvalidateRect(IntPtr hWnd, IntPtr lpRect, bool bErase);
+		[DllImport("user32.dll")]
+		static extern bool InvalidateRect(IntPtr hWnd, IntPtr lpRect, bool bErase);
 
-        [DllImport("user32.dll")]
-        static extern bool UpdateWindow(IntPtr hWnd);
+		[DllImport("user32.dll")]
+		static extern bool UpdateWindow(IntPtr hWnd);
 
-        public static IntPtr GetConsoleWindow01()
-        {
-            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? GetConsoleWindow() : IntPtr.Zero;
-        }
+		public static IntPtr GetConsoleWindow01()
+		{
+			return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? GetConsoleWindow() : IntPtr.Zero;
+		}
 
-        public static void RepaintWindow(IntPtr hWnd)
-        {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                InvalidateRect(hWnd, IntPtr.Zero, true);
+		public static void RepaintWindow(IntPtr hWnd)
+		{
+			if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+			{
+				InvalidateRect(hWnd, IntPtr.Zero, true);
 
-                UpdateWindow(hWnd);
-            }
-        }
-    }
+				UpdateWindow(hWnd);
+			}
+		}
+	}
 }
