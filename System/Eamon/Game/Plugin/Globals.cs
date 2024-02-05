eedac8e5d3585b4322cd2cce46c683bf044cfe52
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2024-02-04 16:09:57 -0800

    Added XML documentation for Globals class.

--- System/Eamon/Game/Plugin/Globals.cs
+++ System/Eamon/Game/Plugin/Globals.cs
@@ -29,7 +29,11 @@ namespace Eamon.Game.Plugin
 			}
 		}
 
-		/// <summary></summary>
+		/// <summary>Gets the current global database.</summary>
+		/// <remarks>
+		/// All game Records reside in this global database, which can be used either directly or through various quick accessors
+		/// (such as <see cref="gRDB">gRDB</see>, etc). The database also exports a variety of services via its many methods.
+		/// </remarks>
 		public static IDatabase gDatabase
 		{
 			get
@@ -39,7 +43,10 @@ namespace Eamon.Game.Plugin
 		}
 
 		/// <summary>Gets the current <see cref="IRoom">Room</see> database.</summary>
-		/// <remarks>You can get or set a specific Room by providing the Room <see cref="IGameBase.Uid">Uid</see> as an indexer.</remarks>
+		/// <remarks>
+		/// The <see cref="IRecordDb{T}.Records">Records</see> property provides access to the Room collection. Additionally, you can 
+		/// get or set a specific Room by using its <see cref="IGameBase.Uid">Uid</see> as an indexer.
+		/// </remarks>
 		public static IRecordDb<IRoom> gRDB
 		{
 			get 
@@ -49,7 +56,10 @@ namespace Eamon.Game.Plugin
 		}
 
 		/// <summary>Gets the current <see cref="IArtifact">Artifact</see> database.</summary>
-		/// <remarks>You can get or set a specific Artifact by providing the Artifact <see cref="IGameBase.Uid">Uid</see> as an indexer.</remarks>
+		/// <remarks>
+		/// The <see cref="IRecordDb{T}.Records">Records</see> property provides access to the Artifact collection. Additionally, you can 
+		/// get or set a specific Artifact by using its <see cref="IGameBase.Uid">Uid</see> as an indexer.
+		/// </remarks>
 		public static IRecordDb<IArtifact> gADB
 		{
 			get 
@@ -59,7 +69,10 @@ namespace Eamon.Game.Plugin
 		}
 
 		/// <summary>Gets the current <see cref="IEffect">Effect</see> database.</summary>
-		/// <remarks>You can get or set a specific Effect by providing the Effect <see cref="IGameBase.Uid">Uid</see> as an indexer.</remarks>
+		/// <remarks>
+		/// The <see cref="IRecordDb{T}.Records">Records</see> property provides access to the Effect collection. Additionally, you can 
+		/// get or set a specific Effect by using its <see cref="IGameBase.Uid">Uid</see> as an indexer.
+		/// </remarks>
 		public static IRecordDb<IEffect> gEDB
 		{
 			get 
@@ -69,7 +82,10 @@ namespace Eamon.Game.Plugin
 		}
 
 		/// <summary>Gets the current <see cref="IMonster">Monster</see> database.</summary>
-		/// <remarks>You can get or set a specific Monster by providing the Monster <see cref="IGameBase.Uid">Uid</see> as an indexer.</remarks>
+		/// <remarks>
+		/// The <see cref="IRecordDb{T}.Records">Records</see> property provides access to the Monster collection. Additionally, you can 
+		/// get or set a specific Monster by using its <see cref="IGameBase.Uid">Uid</see> as an indexer.
+		/// </remarks>
 		public static IRecordDb<IMonster> gMDB
 		{
 			get
