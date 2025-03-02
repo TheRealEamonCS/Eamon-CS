20048200aba5f42f7921f384390286aa43ddad48
TheRealEamonCS <TheRealEamonCS@hotmail.com>
2025-03-01 20:06:10 -0800

    Bug fix: in Orb of My Life, Sagonne can only be healed with his Life Orb.

---
+++ Adventures/OrbOfMyLife/Game/Commands/Interactive/HealCommand.cs
@@ -0,0 +1,32 @@
+﻿
+// HealCommand.cs
+
+// Copyright (c) 2014+ by Michael Penner.  All rights reserved.
+
+using Eamon.Game.Attributes;
+using EamonRT.Framework.Commands;
+using EamonRT.Framework.States;
+using static OrbOfMyLife.Game.Plugin.Globals;
+
+namespace OrbOfMyLife.Game.Commands
+{
+	[ClassMappings]
+	public class HealCommand : EamonRT.Game.Commands.HealCommand, IHealCommand
+	{
+		public override void ExecuteForPlayer()
+		{
+			// Sagonne
+
+			if (DobjMonster?.Uid == 22)
+			{
+				gOut.Print("Sagonne can only be healed with his Life Orb.");
+
+				NextState = gEngine.CreateInstance<IStartState>();
+			}
+			else
+			{
+				base.ExecuteForPlayer();
+			}
+		}
+	}
+}
